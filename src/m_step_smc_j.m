325dc03fdb10332dc6b0286a5ed0ce29ab9d39b5
Ben Shababo <benshababo@gmail.com>
2012-05-05 14:49:14 -0400

    add m_step files for j-only

---
+++ src/m_step_smc_j.m
@@ -0,0 +1,12 @@
+function [theta_intrinsic] = m_step_smc_j(theta_intrinsic, options, varargin)
+
+    %options = optimset(options,'DerivativeCheck', 'on');
+    %options = optimset(options,'FinDiffType', 'central');
+    lb = ones(size(theta_intrinsic)) * -.1;
+    ub = ones(size(theta_intrinsic)) * .1;
+    lb(1) = 0; lb(2) = -5;
+    ub(1) = 5; ub(2) = 5;
+    [theta_intrinsic,fval,exitflag,output] = fmincon('q_single_neuron_j',theta_intrinsic, [], [], [], [], lb, ub,[],options, varargin{:});
+
+end
+

2183f9f8a8cc124b3ec76c8b26e9cc2b06ab4f4f
Ben Shababo <benshababo@gmail.com>
2012-05-06 17:18:35 -0400

    added positive beta main

---
+++ src/m_step_pos_beta.m
@@ -0,0 +1,14 @@
+function [ theta ] = m_step_pos_beta(theta, options, N, beta_bound, w_bound, varargin)
+%M_STEP_FULL Summary of this function goes here
+%   Detailed explanation goes here
+
+    %options = optimset(options, 'DerivativeCheck', 'on');
+    %options = optimset(options,'FinDiffType', 'central');
+    lb = ones(size(theta)) * 0;
+    ub = ones(size(theta)) * beta_bound;
+    lb(1) = 0; lb(2:N+1) = -w_bound;
+    ub(1) = 5; ub(2:N+1) = w_bound;
+    [theta,fval,exitflag,output] = fmincon('q_single_neuron_full',theta, [], [], [], [], lb, ub,[],options, varargin{:});
+
+end
+

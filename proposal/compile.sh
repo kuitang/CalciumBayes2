4b7211f7f33190b1b2bc2de63a58a3fdc1c51705
Kui Tang <kuitang@gmail.com>
2012-05-08 17:35:16 -0400

    added figures

--- proposal/compile.sh
+++ proposal/compile.sh
@@ -1,7 +1,4 @@
 #/bin/sh
 
 PATH=/usr/local/texlive/2010/bin/x86_64-darwin:$PATH
-pdflatex main
-bibtex main
-pdflatex main
-pdflatex main
+pdflatex main && bibtex main && pdflatex main && pdflatex main

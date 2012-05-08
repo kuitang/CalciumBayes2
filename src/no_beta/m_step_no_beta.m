85320976c004e3ec68f5768730c78c65f22e77a2
Ben Shababo <benshababo@gmail.com>
2012-05-07 20:50:22 -0400

    added true no beta version

---
+++ src/no_beta/m_step_no_beta.m
@@ -0,0 +1,14 @@
+function [ theta ] = m_step_no_beta(theta, options, w_bound, varargin)
+%M_STEP_FULL Summary of this function goes here
+%   Detailed explanation goes here
+
+    %options = optimset(options, 'DerivativeCheck', 'on');
+    %options = optimset(options,'FinDiffType', 'central');
+    lb = ones(size(theta)) * -w_bound;
+    ub = ones(size(theta)) * w_bound;
+    lb(1) = 0;
+    ub(1) = 5;
+    [theta,fval,exitflag,output] = fmincon('q_no_beta',theta, [], [], [], [], lb, ub,[],options, varargin{:});
+
+end
+
